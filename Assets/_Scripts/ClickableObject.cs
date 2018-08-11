4206911b4589860962aed11272f92205dc2c2c29
Sponglee <acearutyunyan@gmail.com>
2018-08-11 18:17:59 +0300

    11.08.18
    Initial commit
    
    Working ui system, assets

---
+++ Assets/_Scripts/ClickableObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ClickableObject : MonoBehaviour, IPointerClickHandler
+{
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+            FunctionHandler.Instance.ClickHandler(gameObject.name,0);
+        else if (eventData.button == PointerEventData.InputButton.Right)
+            FunctionHandler.Instance.ClickHandler(gameObject.name,1);
+    }
+}
---
+++ Assets/_Scripts/FunctionHandler.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionHandler : Singleton<FunctionHandler>
+{
+
+    public GameObject menuWindow;
+    public GameObject driveMenu;
+    public GameObject folderMenu;
+
+
+    //Open MainMenu
+    public void OpenMenu()
+    {
+        CloseAllContexts();
+        menuWindow.SetActive(!menuWindow.gameObject.activeSelf);
+        if (Time.timeScale == 1)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+  
+    //Open contextMenu
+    public void OpenContext(string name)
+    {
+        GameObject context = null;
+
+        if(name == "FlashDrive")
+        {
+            context = driveMenu;    
+        }
+        else if (name == "Folder")
+        {
+            context = folderMenu;
+        }
+
+        CloseAllContexts();
+
+        if (context != null)
+        {
+            context.SetActive(!context.gameObject.activeSelf);
+            context.transform.position = Input.mousePosition;
+        }
+       
+    }
+
+    //Close all menus that are open
+    public void CloseAllContexts()
+    {
+        driveMenu.SetActive(false);
+        folderMenu.SetActive(false);
+    }
+
+
+    //Handling clicks on any clickable object
+    //Click index 1- rightclick 0-leftclick
+    public void ClickHandler(string name, int clickIndex)
+    {
+       
+
+        switch (name)
+        {
+            case "FlashDrive":
+                if(clickIndex == 1)
+                    OpenContext("FlashDrive");
+                break;
+            case "Folder":
+                if (clickIndex == 1)
+                    OpenContext("Folder");
+                break;
+           
+            default:
+                CloseAllContexts();
+                break;
+        }
+    }
+}
